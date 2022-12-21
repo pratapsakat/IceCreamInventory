20c96159cc92e89c49587d698c76eee2d20b047e
pratapsakat <111555681+pratapsakat@users.noreply.github.com>
2022-12-21 23:28:50 +0530

    Add files via upload

---
+++ IceCreamInventory/IceCreamInventory/src/main/java/com/example/IceCreamInventory/IceCreamInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.IceCreamInventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IceCreamInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IceCreamInventoryApplication.class, args);
+	}
+
+}
